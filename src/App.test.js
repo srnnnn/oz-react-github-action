b5e47233c36a1faeb525bbb92be892477715f550
srnnnn <tpfla311@naver.com>
2024-05-31 16:57:37 +0900

    build err

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,7 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
+import { act } from 'react';
+
 
 test('the counter starts at 0', () => {
   render(<App />);
