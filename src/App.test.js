149b9a766836a826a907079e8e8699f412077327
srnnnn <tpfla311@naver.com>
2024-05-31 17:31:43 +0900

    0531 commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,5 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
-import { act } from 'react';
 
 test('the counter starts at 0', () => {
   render(<App />);
